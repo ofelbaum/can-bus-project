f69695e08a6c925829f99ffaa0112f8a9b827595
Aleksei Emelianenko <alexey@import2.com>
2017-10-30 01:00:05 +0300

    Add filtering by message ID

--- Arduino/can_bus_reader/can_bus_reader.ino
+++ Arduino/can_bus_reader/can_bus_reader.ino
@@ -14,6 +14,11 @@ Distributed as-is; no warranty is given.
 #include <mcp2515.h>
 #include <mcp2515_defs.h>
 
+byte incomingByte;
+char message[255] = {};
+int index = 0;
+int messageId;
+
 //********************************Setup Loop*********************************//
 
 void setup() {
@@ -32,27 +37,67 @@ void setup() {
 //********************************Main Loop*********************************//
 
 void loop(){
-
+//  printBuffer();
+  initFilter();
   tCAN message;
 if (mcp2515_check_message()) 
   {
     if (mcp2515_get_message(&message)) 
-  {
-        //if(message.id == 0x620 and message.data[2] == 0xFF)  //uncomment when you want to filter
-             //{
+    {
+        if(message.id == messageId || messageId == NULL)
+        {
                
                Serial.print("ID: ");
                Serial.print(message.id,HEX);
                Serial.print(", ");
-               Serial.print("Data: ");
+               Serial.print("Data [");
                Serial.print(message.header.length,DEC);
+               Serial.print(" bytes]: ");
                for(int i=0;i<message.header.length;i++) 
                 { 
                   Serial.print(message.data[i],HEX);
                   Serial.print(" ");
                 }
                Serial.println("");
-             //}
-           }}
+        }
+     }
+  }
+}
 
+void initFilter() {
+if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    if ((int)incomingByte == 10) {
+        // ignore space
+    } else if ((char)incomingByte == '!') {
+        memset(message, 0, sizeof(message));
+        index = 0;
+        messageId = NULL;
+        Serial.println("=============================");
+        Serial.println("=== Filter disabled =========");
+        Serial.println("=============================");
+    } else if ((char)incomingByte == '*') {
+        message[index] = '\0';
+        sscanf(message, "%x", &messageId);
+
+        memset(message, 0, sizeof(message));
+        index = 0;
+        Serial.println("=== Filter enabled ==============");
+        Serial.print("=== Filter on message ID: ");
+        Serial.print(messageId, HEX);
+        Serial.println(" ===");
+        Serial.println("===============================");
+    } else {
+        message[index] = (char)incomingByte;
+        index++;
+    }    
+  }  
+}
+
+void printBuffer() {
+  int i;
+  for(i = 0; i < sizeof(message); i++) {
+    Serial.print(message[i]);
+    }
+    Serial.println();
 }
